e61f203c99d4f1b6e2a39e46ff220c65d038dd28
BerryM2 <berryme@yahoo.com>
2018-10-01 10:20:30 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DemoWeb',
+    version='0.1',
+    packages=['web1.web1'],
+    url='',
+    license='',
+    author='BerryM2',
+    author_email='berryme@yahoo.com',
+    description='Demo django AWS'
+)
