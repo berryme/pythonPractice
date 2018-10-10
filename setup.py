e63dafabc17ef9e06fe6b09f411a945453004942
berryme <berryme@yahoo.com>
2018-10-10 19:38:22 -0400

    more docker changes

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='pythonPractice',
-    version='0.11',
-    packages=['web', 'web.web', 'web.tests', 'web.packages', 'web.packages.xml', 'libs', 'libs.xml', 'web1.web1',
+    version='0.12',
+    packages=['web', 'web.web', 'web.tests', 'web.packages', 'web.packages.xml', 'libs', 'libs.xml', 'web1', 'web1.web1',
               'polls', 'polls.polls'],
     url='',
     license='',
